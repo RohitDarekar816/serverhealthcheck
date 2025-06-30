2011cc2e6656fee14d78adbd86bd25f53857e346
cloudtix <info@cloudtix.in>
2025-06-30 11:35:06 +0000

    chore: removed the echo for memory

--- gitpush.sh
+++ gitpush.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-echo "Available Memory: $(free -h | awk '/Mem:/ {print $7}')" > servermemory.txt
-"CPU Usage: $(top -b -n 1 | grep "Cpu(s)" | awk '{print $2 + $4}')" > servercpu.txt
+
+echo "CPU Usage: $(top -b -n 1 | grep "Cpu(s)" | awk '{print $2 + $4}')" > servercpu.txt
 
 git add .
 git commit -m "chore: update memory usage"
